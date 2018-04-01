65591c9923be57c32116658bcff6a6a881ea4ae9
Kalen Hammann <kalen7@gmail.com>
2018-04-01 14:45:57 -0400

    Add API endpoints for CRUD

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
-var setupController = require
-('./controllers/setupController');
+var setupController = require('./controllers/setupController');
+var apiController = require ('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
@@ -13,5 +13,6 @@ app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString())
 setupController(app);
+apiController(app);
 
 app.listen(port);
